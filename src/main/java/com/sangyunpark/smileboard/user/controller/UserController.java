049367889c0788c27ef281327a34bce9ed5c6fd2
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-09-28 16:18:50 +0900

    [#1] feat: UserController 구현

--- src/main/java/com/sangyunpark/smileboard/user/controller/UserController.java
+++ src/main/java/com/sangyunpark/smileboard/user/controller/UserController.java
@@ -1,65 +1,83 @@
 package com.sangyunpark.smileboard.user.controller;
 
-import com.sangyunpark.smileboard.user.dto.request.UserDto;
-import com.sangyunpark.smileboard.user.dto.request.UserInfoRequest;
+import com.sangyunpark.smileboard.user.dto.UserDto;
 import com.sangyunpark.smileboard.user.dto.request.UserLoginRequest;
+import com.sangyunpark.smileboard.user.dto.request.UserSignupRequest;
 import com.sangyunpark.smileboard.user.dto.request.UserUpdatePasswordRequest;
-import com.sangyunpark.smileboard.user.dto.request.UserWithdrawRequest;
+import com.sangyunpark.smileboard.user.dto.request.UserDeleteRequest;
 import com.sangyunpark.smileboard.user.dto.response.UserInfoResponse;
-import com.sangyunpark.smileboard.user.service.UserServiceImpl;
+import com.sangyunpark.smileboard.user.dto.response.UserLoginResponse;
+import com.sangyunpark.smileboard.user.dto.response.UserLogoutResponse;
+import com.sangyunpark.smileboard.user.dto.response.UserSignupResponse;
+import com.sangyunpark.smileboard.user.dto.response.UserUpdatePasswordResponse;
+import com.sangyunpark.smileboard.user.service.UserService;
+import com.sangyunpark.smileboard.user.utils.SessionUtils;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RestController
-@RequestMapping("/users")
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
-    private final UserServiceImpl userService;
+
+    private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody @Valid UserDto userDto) {
-        userService.signup(userDto);
-        return ResponseEntity.ok("회원 가입을 성공 했습니다.");
+    public ResponseEntity<UserSignupResponse> signup(@Valid  @RequestBody UserSignupRequest request) {
+
+        userService.signup(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new UserSignupResponse());
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
-        userService.login(userLoginRequest.getId(), userLoginRequest.getPassword());
-        return ResponseEntity.ok("로그인을 완료 했습니다.");
+    public ResponseEntity<UserLoginResponse> login(@Valid @RequestBody UserLoginRequest request, HttpSession session) {
+
+        userService.login(request, session);
+
+        return ResponseEntity.ok(new UserLoginResponse());
     }
 
-    @PostMapping("/info")
-    public ResponseEntity<UserInfoResponse> info(@RequestBody @Valid UserInfoRequest request) {
-        UserDto userDto = userService.getUserInfo(request.getId(), request.getPassword());
-        return ResponseEntity.ok(UserInfoResponse.fromUserDto(userDto));
-    }
+    @GetMapping("/info")
+    public ResponseEntity<UserInfoResponse> userInfo(HttpSession session) {
 
-    @PutMapping("/logout")
-    public ResponseEntity<String> logout(@RequestParam String userId) {
-        userService.logout(userId);
-        return ResponseEntity.ok("로그아웃을 완료했습니다.");
-    }
+        UserDto userDto = userService.getUserInfo(session);
+        UserInfoResponse response = UserInfoResponse.fromUserDto(userDto);
 
-    @DeleteMapping
-    public ResponseEntity<String> userWithdraw(@RequestBody @Valid UserWithdrawRequest request) {
-        userService.deleteUser(request.getId(), request.getPassword());
-        return ResponseEntity.ok("계정이 삭제 되었습니다.");
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/password")
-    public ResponseEntity<String> updatePassword(@RequestBody @Valid UserUpdatePasswordRequest request) {
-        userService.updatePassword(request.getId(), request.getPassword(), request.getNewPassword());
-        return ResponseEntity.ok("비밀번호 업데이트를 완료 했습니다.");
+    public ResponseEntity<UserUpdatePasswordResponse> updatePassword(@Valid @RequestBody UserUpdatePasswordRequest request, HttpSession session) {
+
+        userService.updatePassword(request, session);
+
+        return ResponseEntity.ok(new UserUpdatePasswordResponse());
+    }
+
+    @PutMapping("/logout")
+    public ResponseEntity<UserLogoutResponse> logout(HttpSession session) {
+
+        SessionUtils.clear(session);
+
+        return ResponseEntity.ok(new UserLogoutResponse());
+    }
+
+    @DeleteMapping
+    public void delete(@Valid @RequestBody UserDeleteRequest request, HttpSession session) {
+        userService.delete(request,session);
     }
 }
