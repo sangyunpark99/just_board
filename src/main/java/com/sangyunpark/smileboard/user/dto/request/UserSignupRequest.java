31751296b49023b6b41705af6d64444aa31546e5
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-09-29 21:59:55 +0900

    [#1] feat: UserSignupRequest 생성자 추가

--- src/main/java/com/sangyunpark/smileboard/user/dto/request/UserSignupRequest.java
+++ src/main/java/com/sangyunpark/smileboard/user/dto/request/UserSignupRequest.java
@@ -1,15 +1,13 @@
 package com.sangyunpark.smileboard.user.dto.request;
 
+import static com.sangyunpark.smileboard.user.dto.UserStatus.*;
 import static com.sangyunpark.smileboard.user.utils.EncryptUtils.encrypt;
 
 import com.sangyunpark.smileboard.user.domain.User;
-import com.sangyunpark.smileboard.user.dto.UserStatus;
 import com.sangyunpark.smileboard.user.dto.UserType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
 
-@Getter
 public class UserSignupRequest {
 
     @NotBlank
@@ -24,16 +22,28 @@ public class UserSignupRequest {
     @NotNull
     private UserType type;
 
-    @NotNull
-    private UserStatus status;
+    private UserSignupRequest() {
+
+    }
+
+    public UserSignupRequest(final String userId, final String password, final String nickName, final UserType type) {
+        this.userId = userId;
+        this.password = password;
+        this.nickName = nickName;
+        this.type = type;
+    }
 
     public User toEntity() {
         return User.builder()
                 .userId(this.userId)
                 .password(encrypt(this.password))
                 .type(this.type)
-                .status(this.status)
+                .status(IN_USE)
                 .nickName(this.nickName)
                 .build();
+    }
+
+    public String getUserId() {
+        return userId;
     }
 }
