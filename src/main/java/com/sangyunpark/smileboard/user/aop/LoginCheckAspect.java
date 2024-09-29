e0e65c5305a16ccfe46ad2b832634812d2cc7ef4
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-09-29 16:41:45 +0900

    [#1] feat: LoginCheckAspect 구현

---
+++ src/main/java/com/sangyunpark/smileboard/user/aop/LoginCheckAspect.java
@@ -0,0 +1,52 @@
+package com.sangyunpark.smileboard.user.aop;
+
+import static com.sangyunpark.smileboard.user.exception.errorCode.ErrorCode.SESSION_NOT_FOUND;
+
+import com.sangyunpark.smileboard.user.exception.NotFoundSession;
+import com.sangyunpark.smileboard.user.utils.SessionUtils;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Slf4j
+@Component
+@Aspect
+public class LoginCheckAspect {
+
+    @Around("@annotation(com.sangyunpark.smileboard.user.aop.LoginCheck) && @annotation(loginCheck)")
+    public Object adminLoginCheck(ProceedingJoinPoint proceedingJoinPoint, LoginCheck loginCheck) throws Throwable {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+
+        String id = null;
+        int idIndex = 0;
+
+        String userType = loginCheck.type().toString();
+        switch (userType) {
+            case "DEFAULT" :
+                id = SessionUtils.getLoginDefaultId(session);
+                break;
+            case "ADMIN":
+                id = SessionUtils.getLoginAdminId(session);
+                break;
+        }
+
+        if(id == null) {
+            log.error(proceedingJoinPoint.toString() + "userId : " + id);
+            throw new NotFoundSession(SESSION_NOT_FOUND);
+        }
+
+        Object[] modifiedArguments = proceedingJoinPoint.getArgs();
+
+        if(proceedingJoinPoint != null) {
+            modifiedArguments[idIndex] = id;
+        }
+
+        return proceedingJoinPoint.proceed(modifiedArguments);
+    }
+
+}
