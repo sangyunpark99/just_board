b84c6f92ff4fd1c759c90de033f8afa876aa6ddc
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-10-01 10:51:57 +0900

    [#5] feat: Category 엔티티 구현

--- src/main/java/com/sangyunpark/smileboard/category/domain/Category.java
+++ src/main/java/com/sangyunpark/smileboard/category/domain/Category.java
@@ -1,11 +1,16 @@
 package com.sangyunpark.smileboard.category.domain;
 
+import com.sangyunpark.smileboard.board.domain.Post;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Entity
 public class Category {
 
@@ -13,6 +18,9 @@ public class Category {
     private Long id;
 
     private String title;
+
+    @OneToMany(mappedBy = "category")
+    private List<Post> posts;
 
     protected Category(){
 
